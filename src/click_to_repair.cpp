47700ea480995af64bfa991f282fba547e8e4fc8
Marius Elvert <marius.elvert@softwareschneiderei.de>
2020-01-31 21:17:33 +0100

    Try to handle a keypress

--- src/click_to_repair.cpp
+++ src/click_to_repair.cpp
@@ -1,6 +1,17 @@
+// Oak Nut include
+#include <onut/Anim.h>
+#include <onut/Renderer.h>
+#include <onut/Settings.h>
+#include <onut/Sound.h>
+#include <onut/SpriteBatch.h>
+#include <onut/Texture.h>
+#include <onut/Input.h>
+#include <onut/Window.h>
+#include <onut/onut.h>
 
 void initSettings()
 {
+    oSettings->setGameName("Click to repair!");
 }
 
 void init()
@@ -9,10 +20,15 @@ void init()
 
 void update()
 {
+  if (OInputJustPressed(OKeyEscape))
+    OQuit();
+    
 }
 
 void render()
 {
+    oRenderer->clear(OColorHex(FFFFFF));
+
 }
 
 void postRender()
