5fc9ce6d2d772b7c7040ad8dea8fdfb9aec78e93
Sven Möllers <sven.moellers+git@gmail.com>
2020-02-01 12:58:41 +0100

    previous build commit was incomplete

--- conanfile.py
+++ conanfile.py
@@ -5,10 +5,15 @@ class ClickToRepairConan(ConanFile):
     generators = "cmake"
 
     def requirements(self):
-        if self.settings.os == "Linux":
+        requirements = []
+        if self.settings.os != "Linux":
             # Please install glew and SDL2 from system libraries
             # e.g. 
             #   sudo apt-get install libglew-dev libsdl2-dev
-            self.requires("assimp/5.0.1", "entt/3.2.2" )
+            requirements = ["assimp/5.0.1","entt/3.2.2"]
         else:
-            self.requires("glew/2.1.0@bincrafters/stable","sdl2/2.0.10@bincrafters/stable","assimp/5.0.1", "entt/3.2.2")
+            requirements = ["assimp/5.0.1", "entt/3.2.2",
+                "glew/2.1.0@bincrafters/stable","sdl2/2.0.10@bincrafters/stable"]
+
+        for x in requirements:
+            self.requires(x)
