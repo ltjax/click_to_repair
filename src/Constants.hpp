5b85e46fffd8ebb7d486ea30ee3bf64155f7c492
Marius Elvert <marius.elvert@softwareschneiderei.de>
2020-02-02 13:56:58 +0100

    Tweaking

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -6,12 +6,12 @@ namespace Constants
 {
     Color BackgroundColor();
 
-    inline constexpr auto REPAIR_RESOURCE_DRAIN_PER_SECOND = 0.025f;
-    inline constexpr auto REPAIR_DURATION_INCREASE_PER_SECOND_WITH_RESOURCE = 0.25f;
-    inline constexpr auto REPAIR_DURATION_INCREASE_PER_SECOND_WHEN_EMPTY = 0.15f;
+    inline constexpr auto REPAIR_RESOURCE_DRAIN_PER_SECOND = 0.05f;
+    inline constexpr auto REPAIR_DURATION_INCREASE_PER_SECOND_WITH_RESOURCE = 0.5f;
+    inline constexpr auto REPAIR_DURATION_INCREASE_PER_SECOND_WHEN_EMPTY = 0.3f;
     inline constexpr auto REPAIR_UP_FRONT_COST = 0.05f;
 
-    inline constexpr auto GEAR_WEAR_PER_SECOND = 0.075f;
+    inline constexpr auto GEAR_WEAR_PER_SECOND = 0.070f;
     inline constexpr auto ENGINE_WEAR_PER_SECOND_SHAKING = 0.3f;
     inline constexpr auto ENGINE_WEAR_PER_SECOND = 0.05f;
     inline constexpr auto ENGINE_SHAKE_DURATION = std::chrono::milliseconds(1200);
