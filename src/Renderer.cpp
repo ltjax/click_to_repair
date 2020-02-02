8b58089b85b77186f501f8245b76c64f3c5e0c3c
Fabian Sperber <fabian.sperber@gmx.net>
2020-02-02 05:53:01 +0100

    Fix

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -107,7 +107,7 @@ void renderCursor(GameState const& state)
     const float targetSize = 96;
     oRenderer->renderStates.blendMode = OBlendAlpha;
     static float rot_time;
-    auto s = std::isinf(state.repair_time * 7.f);
+    auto s = std::sin(state.repair_time * 7.f);
     float rotation = (s < 0 ? 1.f : -1.f) * (s*s) * 25.f;
     oSpriteBatch->drawSprite(icon_wrench,
         oInput->mousePosf,
